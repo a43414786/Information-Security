de5b25fab5f0ba7b318e0f2bfb393d8eaf136e6b
a43414786 <72087693+a43414786@users.noreply.github.com>
2022-10-03 10:22:43 +0800

    Update des.py

--- HW3/des.py
+++ HW3/des.py
@@ -1,34 +1,24 @@
-import binascii
-from pyDes import des, CBC, PAD_PKCS5
+from Crypto.Cipher import DES
+
+key = b'abcdefgh'  # 密钥 8位或16位,必须为bytes
 
 
-def des_encrypt(s):
-    """
-    DES 加密
-    :param s: 原始字符串
-    :return: 加密后字符串，16进制
-    """
-    secret_key = '20171117'
-    iv = secret_key
-    k = des(secret_key, CBC, iv, pad=None, padmode=PAD_PKCS5)
-    en = k.encrypt(s, padmode=PAD_PKCS5)
-    return binascii.b2a_hex(en)
+def pad(text):
+    # 如果text不是8的倍数【加密文本text必须为8的倍数！】，补足为8的倍数
+    while len(text) % 8 != 0:
+        text += ' '
+    return text
 
 
-def des_descrypt(s):
-    """
-    DES 解密
-    :param s: 加密后的字符串，16进制
-    :return:  解密后的字符串
-    """
-    secret_key = '20171117'
-    iv = secret_key
-    k = des(secret_key, CBC, iv, pad=None, padmode=PAD_PKCS5)
-    de = k.decrypt(binascii.a2b_hex(s), padmode=PAD_PKCS5)
-    return de
+des = DES.new(key, DES.MODE_ECB)  # 创建DES实例
+text = 'Python rocks!'
+padded_text = pad(text)
 
+encrypted_text = des.encrypt(padded_text.encode('utf-8'))  # 加密
+print(encrypted_text)
+# b'>\xfc\x1f\x16x\x87\xb2\x93\x0e\xfcH\x02\xd59VQ'
 
-str_en = des_encrypt('zx')
-print(str_en)
-str_de = des_descrypt(str_en)
-print(str_de)
+plain_text = des.decrypt(encrypted_text).decode().rstrip(' ')  # 解密
+print(plain_text)
+# Python rocks!
+# YYYYiJceGQ3Q1JceGI0XHgxMFx4MWJceGU1XHhiMlZceDE1XHgxZEZJXHhkMFx4OTg8XHhlNlx4ZDd4OVx4ZTdceGY2R1x4MTJceGZiXHg4OVx4ZTFceGNiXHg5OC5ceGRjdFx4MDZceGVkdU96XHhkNFx4OTBceDExXHhkMDQnXHhhNFx0TnEkXHhmZXtceDlhXHhjOVx4YjJceDA4XHhjNDYiYYY
